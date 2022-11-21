1fe9345056829e52753ef3ff0001574ca6e6054e
fernandocamacho <salvadorfernando200@outlook.com>
2022-11-21 10:22:46 -0600

    Generar numeros random en json

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
 from flask import Flask, request, jsonify
 import json
+import random
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    puntos = []
+    # loop para generar carritos internos
+    for i in range(10):
+        puntos.append({"id": i,
+                       "x": random.uniform(0, 10),
+                       "y": random.uniform(0, 10),
+                       "z": random.uniform(0, 10)})
 
-@app.route("/save_json", methods=["GET", "POST"])
-def save_json_file():
-    data = request.json()
-    print(data)
-    with open("json_file.json", "w+") as f:
-        f.write(json.dumps(data, indent=4))
-    return "JSON saved"
+        # COMO SACAR VALORES RANDOM
+        with open("json_file.json", "w+") as f:
+            f.write(json.dumps({'cars' : puntos}, indent=4))
 
-@app.route("/get_json", methods=["GET", "POST"])
-def get_json_file():
-    with open("json_file.json", "w+") as f:
-        data = f.readlines()
-    data = "\n".join(data) #esto es cadena
-    data = json.loads(data) #esto es diccionario
-    return jsonify(data)
+    return {'cars': puntos}
+
+# @app.route('/save_json', methods=["GET", "POST"])
+# def save_json_file():
+#     data = request.json()
+#     print(data)
+#     with open("json_file.json", "w+") as f:
+#         f.write(json.dumps(data, indent=4))
+#     return "JSON saved"
+
+# @app.route('/get_json', methods=["GET", "POST"])
+# def get_json_file():
+#     with open("json_file.json", "w+") as f:
+#         data = f.readlines()
+#     data = "\n".join(data) #esto es cadena
+#     data = json.loads(data) #esto es diccionario
+#     return jsonify(data)
 
 
 if __name__ == "__main__":
